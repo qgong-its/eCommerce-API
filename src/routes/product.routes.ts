6b1b8456403209740ed90b4e8b6b98193d35c6ed
Qian Gong <gongqian.de@gmail.com>
2026-05-20 03:11:14 +0200

    feat(route): add validated crud routes for ecommerce resources

---
+++ src/routes/product.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+} from '#controllers';
+
+import { basicErrorHandler, extendedErrorHandler, validate } from '#middleware';
+import {
+  createProductZodSchema,
+  updateProductZodSchema,
+  productIdParamsZodSchema,
+} from '#schemas';
+
+const productRouter = Router();
+
+productRouter
+  .route('/')
+  .get(getProducts)
+  .post(validate(createProductZodSchema), createProduct);
+
+productRouter
+  .route('/:id')
+  .get(validate(productIdParamsZodSchema, 'params'), getProductById)
+  .patch(
+    validate(productIdParamsZodSchema, 'params'),
+    validate(updateProductZodSchema),
+    updateProduct,
+  )
+  .delete(validate(productIdParamsZodSchema, 'params'), deleteProduct);
+
+export default productRouter;
