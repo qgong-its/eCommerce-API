691ffc0fa95ce3c82f2e978541abcb77dcc7e41f
Qian Gong <gongqian.de@gmail.com>
2026-05-20 10:55:21 +0200

    chore(deploy): prepare app for render deployment

--- src/middleware/corsHandler.ts
+++ src/middleware/corsHandler.ts
@@ -2,10 +2,10 @@ import cors from 'cors';
 
 const allowedOrigins =
   process.env.NODE_ENV === 'production'
-    ? [process.env.FRONTEND_URL as string]
+    ? process.env.FRONTEND_URL?.split(',') ?? []
     : ['http://localhost:5173'];
 
 export const corsHandler = cors({
-  origin: allowedOrigins,
+  origin: allowedOrigins.length > 0 ? allowedOrigins : true,
   credentials: true,
 });
